c39a716a17bed9ee9fa55351c57527c298fb9a84
Nobu <chat@nobu.sh>
2022-07-03 10:26:25 -0500

    refactor(typescript): Update ft

---
+++ src/pixelit.ts
@@ -0,0 +1,8 @@
+/**
+ * pixelit - convert an image to Pixel Art, with/out grayscale and based on a color palette.
+ * @author José Moreira @ <https://github.com/giventofly/pixelit>
+ **/
+
+class pixelit {
+  
+}
